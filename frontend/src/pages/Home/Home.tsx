9aa4546d7d8ed8e2a839c65e57ce090e4e3f40b3
imjh990614 <imjh990614@gmail.com>
2025-04-22 11:17:55 +0900

    feat: 이전 버전 home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Home() {
   const backgrounds = [
@@ -11,6 +11,7 @@ function Home() {
   const [currentBgIndex, setCurrentBgIndex] = useState(0);
   const [prevBgIndex, setPrevBgIndex] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
+  const rippleRefs = useRef<HTMLDivElement[]>([]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -38,13 +39,18 @@ function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-between h-screen relative">
+    <div className="flex flex-col items-center justify-between h-screen relative ripple-container">
+      <div
+        ref={(el) => {
+          if (el) rippleRefs.current[0] = el;
+        }}
+        className="ripple"
+      ></div>
       <main className="w-screen h-full">
         {/* 현재 배경 - 페이드 인 효과 */}
         <div
-          className={`w-full h-full z-[-2] fixed background duration-300 ease-in-out transition-opacity ${
-            backgrounds[currentBgIndex]
-          } ${
+          className={`w-full h-full z-[-2] fixed duration-300 ease-in-out transition-opacity background-dark
+            ${backgrounds[currentBgIndex]} ${
             isTransitioning
               ? "transition-none opacity-0"
               : "transition-opacity opacity-100"
@@ -52,18 +58,41 @@ function Home() {
           `}
         ></div>
         <div
-          className={`w-full h-full z-[-3] fixed background ${backgrounds[prevBgIndex]}`}
+          className={`w-full h-full z-[-3] fixed background-dark ${backgrounds[prevBgIndex]}`}
         ></div>
+        <div
+          className={`w-[400%] h-full fixed flex transition-transform duration-500 ease-in-out`}
+          style={{ transform: `translateX(-${currentBgIndex * 25}%)` }}
+        >
+          <div className="w-1/4 h-[calc(100%-4rem)] flex flex-col items-center justify-center">
+            <div className="w-2/5 h-2/5 mb-6"></div>
+            <div className="text-4xl font-gothic">
+              <span>인적 사항 입력</span>
+            </div>
+          </div>
+          <div className="w-1/4 h-[calc(100%-4rem)] flex flex-col items-center justify-center">
+            <div className="w-2/5 h-2/5 mb-6"></div>
+            <div className="text-4xl font-gothic">기업 분석 / 직무 분석</div>
+          </div>
+          <div className="w-1/4 h-[calc(100%-4rem)] flex flex-col items-center justify-center">
+            <div className="w-2/5 h-2/5 mb-6"></div>
+            <div className="text-4xl font-gothic">자기소개서 작성</div>
+          </div>
+          <div className="w-1/4 h-[calc(100%-4rem)] flex flex-col items-center justify-center">
+            <div className="w-2/5 h-2/5 mb-6"></div>
+            <div className="text-4xl font-gothic">면접 준비</div>
+          </div>
+        </div>
 
         {/* 왼쪽 화살표 버튼 */}
         <button
           onClick={handlePrevClick}
-          className="absolute left-5 top-1/2 transform -translate-y-1/2 text-white opacity-50 z-10 w-32 h-32 flex items-center justify-center bg-transparent cursor-pointer hover:opacity-60 transition-opacity"
+          className="absolute left-5 top-1/2 transform -translate-y-1/2 text-white opacity-40 z-10 w-28 h-28 flex items-center justify-center bg-transparent cursor-pointer hover:opacity-50 transition-opacity"
           disabled={isTransitioning}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-32 w-32"
+            className="h-28 w-28"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -81,12 +110,12 @@ function Home() {
         {/* 오른쪽 화살표 버튼 */}
         <button
           onClick={handleNextClick}
-          className="absolute right-5 top-1/2 transform -translate-y-1/2 text-white opacity-50 z-10 w-32 h-32 flex items-center justify-center bg-transparent cursor-pointer hover:opacity-60 transition-opacity"
+          className="absolute right-5 top-1/2 transform -translate-y-1/2 text-white opacity-40 z-10 w-28 h-28 flex items-center justify-center bg-transparent cursor-pointer hover:opacity-50 transition-opacity"
           disabled={isTransitioning}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-32 w-32"
+            className="h-28 w-28"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -101,7 +130,7 @@ function Home() {
           </svg>
         </button>
       </main>
-      <footer className="flex items-center fixed bottom-0 w-full h-16 bg-gray-950/60">
+      <footer className="flex items-center fixed bottom-0 w-full h-12 bg-gray-950/60">
         <div className="text-white">{currentBgIndex}</div>
       </footer>
     </div>
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
 import useAuthStore from "@/store/authStore";
+import Logo from "./components/Logo";
 
 function Login() {
   const { isLogin, login, logout } = useAuthStore();
@@ -6,8 +7,9 @@ function Login() {
   return (
     <div className="flex flex-col items-center justify-between h-screen relative">
       <main className="w-screen h-full">
-        <div className="w-full h-full fixed z-[-2] background login-background"></div>
-        <div>
+        <div className="w-full h-full fixed z-[-2] background-dark login-background"></div>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <Logo />
           <h1>라우터 테스트</h1>
           {isLogin ? (
             <button onClick={logout}>로그아웃</button>
