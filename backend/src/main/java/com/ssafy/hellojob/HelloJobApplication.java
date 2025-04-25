ff80abb523e3320a68ff97bcd097313541e825a3
이유리 <yetcome0123@gmail.com>
2025-04-25 16:56:46 +0900

    docs: 백엔드 프로젝트명 변경
    backend -> hellojob으로 변경

--- backend/src/main/java/com/ssafy/hellojob/HelloJobApplication.java
+++ backend/src/main/java/com/ssafy/hellojob/HelloJobApplication.java
@@ -1,13 +1,13 @@
-package com.ssafy.backend;
+package com.ssafy.hellojob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class HelloJobApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(HelloJobApplication.class, args);
 	}
 
 }
