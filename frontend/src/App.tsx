2b3bcffcd4ceb36982f2e38061558914e0064ee6
kub938 <kub938@naver.com>
2025-04-25 17:13:39 +0900

    design: 메인 w사이즈 1280px 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,15 @@ import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
-    <>
-      <Outlet />
-      <Toaster />
-    </>
+    <div id="wrap">
+      <header className="max-w-screen-xl mx-auto border">
+        안녕하세요? 헤더입니다.
+      </header>
+      <main className="max-w-screen-xl mx-auto border">
+        <Outlet />
+        <Toaster />
+      </main>
+    </div>
   );
 }
 
