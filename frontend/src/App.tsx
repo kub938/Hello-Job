d907062da54758372bd050666b5eee58365e9ceb
kub938 <kub938@naver.com>
2025-04-27 23:56:39 +0900

    feat: header component 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router";
 import { Toaster } from "@/components/ui/sonner";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div id="wrap">
-      <header className="max-w-screen-xl mx-auto border">
-        안녕하세요? 헤더입니다.
-      </header>
-      <main className="max-w-screen-xl mx-auto border">
+    <div id="wrap" className="bg-background min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow max-w-screen-xl mx-auto w-full">
         <Outlet />
         <Toaster />
       </main>
