e81d5c5f4617c3cbd8c4507e3bf84cd9340b2ad2
이유리 <yetcome0123@gmail.com>
2025-04-27 20:23:15 +0900

    feat: BaseTimeEntity 구현
    created_at, updated_at 자동 생성 설정

--- backend/src/main/java/com/ssafy/hellojob/HelloJobApplication.java
+++ backend/src/main/java/com/ssafy/hellojob/HelloJobApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.hellojob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HelloJobApplication {
 
 	public static void main(String[] args) {
