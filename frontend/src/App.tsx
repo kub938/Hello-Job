2bb1d0947dfe11ea036883de57f9328685b26d01
imjh990614 <imjh990614@gmail.com>
2025-04-29 13:42:35 +0900

    feat: layout을 통한 유동적인 router page component 출력 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router";
 import { Toaster } from "@/components/ui/sonner";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div id="wrap" className="bg-background min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow max-w-screen-xl mx-auto w-full">
-        <Outlet />
-        <Toaster />
-      </main>
+      <Outlet />
+      <Toaster />
     </div>
   );
 }
