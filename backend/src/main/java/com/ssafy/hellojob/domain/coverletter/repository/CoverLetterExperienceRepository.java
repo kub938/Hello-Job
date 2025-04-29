cd7c151aa3bc064edaa2f7757f0facec512461ab
sunny <rjqnrdl5113@naver.com>
2025-04-29 21:16:41 +0900

    feat: 자기소개서 관련 레포지토리 생성
    #90

---
+++ backend/src/main/java/com/ssafy/hellojob/domain/coverletter/repository/CoverLetterExperienceRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.hellojob.domain.coverletter.repository;
+
+import com.ssafy.hellojob.domain.coverletter.entity.CoverLetterExperience;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoverLetterExperienceRepository extends JpaRepository<CoverLetterExperience, Integer> {
+}
