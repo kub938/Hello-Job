6bcf559a6be81d5fbf45c09b47d94572ada9c143
sunny <rjqnrdl5113@naver.com>
2025-04-29 21:15:44 +0900

    feat: 자기소개서 작성 요청 dto 생성
    #90

---
+++ backend/src/main/java/com/ssafy/hellojob/domain/coverletter/dto/request/CoverLetterRequestDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.hellojob.domain.coverletter.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoverLetterRequestDto {
+    private Long companyAnalysisId;
+    private Integer jobRoleAnalysisId;
+    private List<ContentsDto> contents;
+}
