fc157af8fa1842f8875ebe549e69e5c4382e984b
kub938 <kub938@naver.com>
2025-04-29 12:24:32 +0900

    design: FormInput 컴포넌트 이름, style 변경

--- frontend/src/components/Common/FormInput.tsx
+++ frontend/src/components/Common/FormInput.tsx
@@ -1,4 +1,4 @@
-interface InputProps {
+interface FormInputProps {
   type:
     | "text"
     | "email"
@@ -7,9 +7,10 @@ interface InputProps {
     | "tel"
     | "data"
     | "range"
-    | "password";
-  width: number;
-  height: number;
+    | "password"
+    | "date";
+  width: string;
+  height: string;
   name: string;
   className?: string;
   label?: string;
@@ -17,7 +18,7 @@ interface InputProps {
   placeholder?: string;
 }
 
-function Input({
+function FormInput({
   type,
   name,
   className,
@@ -26,13 +27,13 @@ function Input({
   width,
   height,
   placeholder,
-}: InputProps) {
+}: FormInputProps) {
   return (
-    <>
+    <div className="my-2">
       {label && (
         <>
           <label htmlFor=""></label>
-          <div className="text-foreground mb-1">
+          <div className="text-foreground text-sm font-semibold mb-2">
             {label} {require && <span className="text-destructive">*</span>}
           </div>
         </>
@@ -43,10 +44,11 @@ function Input({
         name={name}
         required={require === true}
         placeholder={placeholder}
-        className={`w-${width} h-${height} border-2 px-3 py-2 rounded-md outline-none not-placeholder-shown:invalid:ring not-placeholder-shown:invalid:border-none not-placeholder-shown:invalid:ring-destructive focus:ring-2 focus:ring-accent focus:border-none focus:invalid:ring-destructive focus:invalid:ring-2 `}
+        style={{ width, height }}
+        className={`border-1 px-3 py-2 rounded-md outline-none not-placeholder-shown:invalid:ring not-placeholder-shown:invalid:border-none not-placeholder-shown:invalid:ring-destructive focus:ring-2 focus:ring-accent focus:border-none focus:invalid:ring-destructive focus:invalid:ring-2 ${className}`}
       />
-    </>
+    </div>
   );
 }
 
-export default Input;
+export default FormInput;
