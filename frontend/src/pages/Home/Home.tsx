50304811f6466c71aa0a1242b652f228f8422452
imjh990614 <imjh990614@gmail.com>
2025-04-29 16:24:46 +0900

    feat: 모달 컴포넌트 개발 완료

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,24 @@
+import { Button } from "@/components/Button";
+import Modal from "@/components/Modal";
+import { useState } from "react";
+
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="flex flex-col flex-grow items-center justify-between"></div>
+    <div className="flex flex-col flex-grow items-center justify-between">
+      <Button onClick={() => setIsModalOpen(true)}>모달 열기</Button>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={() => setIsModalOpen(false)}
+        title="모달 제목"
+      >
+        <p>
+          모달 콘텐츠가 여기에 표시됩니다.모달 콘텐츠가 여기에 표시됩니다.모달
+          콘텐츠가 여기에 표시됩니다.모달 콘텐츠가 여기에 표시됩니다.모달
+        </p>
+      </Modal>
+    </div>
   );
 }
 
