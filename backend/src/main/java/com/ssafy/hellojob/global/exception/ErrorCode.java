5c3dfdcf1f1e79bbd496ea20fd178381c5d703ae
sunny <rjqnrdl5113@naver.com>
2025-04-29 09:22:43 +0900

    feat: 경험 예외 처리를 위한 에러 코드 정의
    #87

--- backend/src/main/java/com/ssafy/hellojob/global/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/hellojob/global/exception/ErrorCode.java
@@ -29,6 +29,9 @@ public enum ErrorCode {
     PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 프로젝트를 찾을 수 없습니다."),
     PROJECT_MISMATCH(HttpStatus.BAD_REQUEST, "현재 유저와 프로젝트 입력자가 일치하지 않습니다."),
 
+    // 경험
+    EXPERIENCE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 경험을 찾을 수 없습니다."),
+    EXPERIENCE_MISMATCH(HttpStatus.BAD_REQUEST, "현재 유저와 경험 입력자가 일치하지 않습니다."),
 
     /**
      Response의 에러 코드에 맞춰 HttpStatus를 설정해주시기 바랍니다.
