e2b1b1d67b807f6cb0ceacf0c4f15fd2c6c29c89
이유리 <yetcome0123@gmail.com>
2025-04-30 15:45:06 +0900

    docs: msater 브렌치에서 ai, backend 폴더 가져옴

---
+++ backend/src/main/java/com/ssafy/hellojob/HelloJobApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.hellojob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJobApplication.class, args);
+	}
+
+}
