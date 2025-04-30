f0940fd3c39e03621ec8839a3646cb20c0a17218
이유리 <yetcome0123@gmail.com>
2025-04-30 15:56:43 +0900

    docs: msater 브렌치에서 frontend, backend 폴더 가져옴

---
+++ backend/src/test/java/com/ssafy/hellojob/HelloJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hellojob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
