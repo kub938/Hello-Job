32b9ee9d722f00dabdbad57219867838226164fd
Jaehwan <woghks1213@gmail.com>
2025-05-02 10:17:47 +0900

    feat: API 버전 관리 및 테스트 라우터 추가
    - API 경로를 "/api/v1"에서 "/api/v1/ai"로 변경
    - tests 라우터를 추가하여 API 테스트 기능 통합
    
    #S12P31B105-180

--- ai/main.py
+++ ai/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import company_analysis
+from app.api.v1.endpoints import company_analysis, tests
 
 app = FastAPI()
 
-# 라우터 추가
-app.include_router(company_analysis.router, prefix="/api/v1")
+API_V1_STR = "/api/v1/ai"
 
-@app.get("/")
+# 라우터 추가
+app.include_router(company_analysis.router, prefix=API_V1_STR)
+app.include_router(tests.router, prefix=API_V1_STR)
+
+@app.get(f"{API_V1_STR}")
 async def root():
     return {"message": "Hello Job"} 
