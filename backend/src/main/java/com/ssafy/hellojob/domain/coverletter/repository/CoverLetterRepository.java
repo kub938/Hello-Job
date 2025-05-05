ce42979f795014669bcf932a8fe335fa722989c5
sunny <rjqnrdl5113@naver.com>
2025-05-05 19:46:14 +0900

    feat: 마이페이지 자기소개서 목록 조회
    #70

--- backend/src/main/java/com/ssafy/hellojob/domain/coverletter/repository/CoverLetterRepository.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/coverletter/repository/CoverLetterRepository.java
@@ -1,15 +1,30 @@
 package com.ssafy.hellojob.domain.coverletter.repository;
 
+import com.ssafy.hellojob.domain.coverletter.dto.response.MyPageCoverLetterDto;
 import com.ssafy.hellojob.domain.coverletter.entity.CoverLetter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+
 public interface CoverLetterRepository extends JpaRepository<CoverLetter, Integer> {
     @Modifying
     @Transactional
     @Query("UPDATE CoverLetter c SET c.updatedAt = CURRENT_TIMESTAMP WHERE c.coverLetterId = :coverLetterId")
     void touch(@Param("coverLetterId") Integer coverLetterId);
+
+    @Query("""
+            SELECT new com.ssafy.hellojob.domain.coverletter.dto.response.MyPageCoverLetterDto(
+            cl.coverLetterId, c.companyName, jr.jobRoleSnapshotName, jr.jobRoleSnapshotCategory, cl.finish, cl.updatedAt)
+            FROM CoverLetter cl
+            JOIN cl.jobRoleSnapshot jr
+            JOIN cl.companyAnalysis ca
+            JOIN ca.company c
+            WHERE cl.user.userId = :userId
+            """)
+    Page<MyPageCoverLetterDto> getCoverLettersByUser(@Param("userId") Integer userId, Pageable pageable);
 }
--- backend/src/main/java/com/ssafy/hellojob/domain/user/controller/MypageController.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/user/controller/MypageController.java
@@ -1,13 +1,20 @@
 package com.ssafy.hellojob.domain.user.controller;
 
+import com.ssafy.hellojob.domain.coverletter.dto.response.MyPageCoverLetterDto;
+import com.ssafy.hellojob.domain.coverletter.service.CoverLetterService;
 import com.ssafy.hellojob.domain.jobroleanalysis.dto.JobRoleAnalysisSearchListResponseDto;
 import com.ssafy.hellojob.domain.jobroleanalysis.service.JobRoleAnalysisService;
 import com.ssafy.hellojob.global.auth.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,9 +25,10 @@ import java.util.List;
 public class MypageController {
 
     private final JobRoleAnalysisService jobRoleAnalysisService;
+    private final CoverLetterService coverLetterService;
 
     @GetMapping("/job-role-analysis")
-    public ResponseEntity<?> JobRoleAnalysisListSearchByUser(@AuthenticationPrincipal UserPrincipal userPrincipal){
+    public ResponseEntity<?> JobRoleAnalysisListSearchByUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
         Integer userId = userPrincipal.getUserId();
         List<JobRoleAnalysisSearchListResponseDto> result = jobRoleAnalysisService.searchJobRoleAnalysisByUserId(userId);
@@ -29,6 +37,18 @@ public class MypageController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/cover-letter")
+    public ResponseEntity<?> getCoverLetterForMyPage(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @PageableDefault(size = 10, sort = "updatedAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Integer userId = principal.getUserId();
+        Page<MyPageCoverLetterDto> page = coverLetterService.getCoverLettersForMaPage(userId, pageable);
+
+        return page.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(page);
     }
 
 }
