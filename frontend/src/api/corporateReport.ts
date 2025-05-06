5b87d8022c12f38e841cd9a67bfea6fb9ed9efd5
imjh990614 <imjh990614@gmail.com>
2025-05-07 02:12:07 +0900

    feat: api 관련 코드

--- frontend/src/api/corporateReport.ts
+++ frontend/src/api/corporateReport.ts
@@ -1,7 +1,27 @@
+import { postBookmarkRequest } from "@/types/coporateResearch";
 import { authApi } from "./instance";
+
+export const corporateListApi = {
+  getCorporateList: (companyName: string) => {
+    return authApi.get(`/api/v1/company/search?companyName=${companyName}`);
+  },
+};
 
 export const corporateReportApi = {
   getCorporateReportList: (companyId: number) => {
     return authApi.get(`/api/v1/company-analysis/search/${companyId}`);
+  },
+  getCorporateReportDetail: (companyAnalysisId: number) => {
+    return authApi.get(`/api/v1/company-analysis/${companyAnalysisId}`);
+  },
+  postBookmark: (postBookmarkRequest: postBookmarkRequest) => {
+    return authApi.post(`/api/v1/company-analysis/bookmark`, {
+      postBookmarkRequest,
+    });
+  },
+  deleteBookmark: (companyAnalysisId: number) => {
+    return authApi.delete(
+      `/api/v1/company-analysis/bookmark/${companyAnalysisId}`
+    );
   },
 };
