20540b8af93c9a9a7573cc4f86a684e0f0db2e0c
sunny <rjqnrdl5113@naver.com>
2025-05-07 20:41:32 +0900

    fix: API 명세서에 맞게 변수명 수정
    #90

--- backend/src/main/java/com/ssafy/hellojob/domain/coverletter/dto/ai/request/AICoverLetterRequestDto.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/coverletter/dto/ai/request/AICoverLetterRequestDto.java
@@ -10,13 +10,13 @@ import java.util.List;
 @NoArgsConstructor
 public class AICoverLetterRequestDto {
     private CompanyAnalysisDto company_analysis;
-    private JobRoleAnalysisDto jobRole_analysis;
+    private JobRoleAnalysisDto job_role_analysis;
     private List<ContentDto> contents;
 
     @Builder
-    public AICoverLetterRequestDto(CompanyAnalysisDto company_analysis, JobRoleAnalysisDto jobRole_analysis, List<ContentDto> contents) {
+    public AICoverLetterRequestDto(CompanyAnalysisDto company_analysis, JobRoleAnalysisDto job_role_analysis, List<ContentDto> contents) {
         this.company_analysis = company_analysis;
-        this.jobRole_analysis = jobRole_analysis;
+        this.job_role_analysis = job_role_analysis;
         this.contents = contents;
     }
 }
