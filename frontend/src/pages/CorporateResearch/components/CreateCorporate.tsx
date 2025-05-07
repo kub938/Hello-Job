8b1c96b380ec20851fab51ec11de34ccf0f77e7e
imjh990614 <imjh990614@gmail.com>
2025-05-07 13:38:49 +0900

    fix: 기업 분석 생성 페이지 기본 프레임

--- frontend/src/pages/CorporateResearch/components/CreateCorporate.tsx
+++ frontend/src/pages/CorporateResearch/components/CreateCorporate.tsx
@@ -1,7 +1,15 @@
-interface CreateCorporateProps {}
+import { Button } from "@/components/Button";
 
-function CreateCorporate() {
-  return <>생성</>;
+interface CreateCorporateProps {
+  onClose: () => void;
+}
+
+function CreateCorporate({ onClose }: CreateCorporateProps) {
+  return (
+    <div className="h-[90vh] w-[940px] bg-white rounded-t-xl py-8 px-12 overflow-y-auto">
+      <Button onClick={onClose}>닫기</Button>
+    </div>
+  );
 }
 
 export default CreateCorporate;
