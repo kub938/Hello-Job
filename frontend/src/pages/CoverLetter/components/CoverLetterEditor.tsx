68de306e8cb2e0e199a7d1a354afa1a308cc8344
kub938 <kub938@naver.com>
2025-05-08 17:01:27 +0900

    chore: 데이터 구조 변경에 따른 수정

--- frontend/src/pages/CoverLetter/components/CoverLetterEditor.tsx
+++ frontend/src/pages/CoverLetter/components/CoverLetterEditor.tsx
@@ -4,63 +4,25 @@ import { useGetCoverLetter } from "@/hooks/coverLetterHooks";
 import { CoverLetterResponse } from "@/types/coverLetterTypes";
 import { useState } from "react";
 
-export interface CoverLetterEditor {
-  selectQuestion: number;
-  coverLetterId: number;
+export interface CoverLetterEditorProps {
+  CoverLetterData: CoverLetterResponse;
   onChangeContentDetail: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   nowContentLength: number;
 }
 
 function CoverLetterEditor({
-  selectQuestion,
-  coverLetterId,
+  CoverLetterData,
   onChangeContentDetail,
   nowContentLength,
-}: CoverLetterEditor) {
-  const { data, isLoading, isError } = useGetCoverLetter(selectQuestion);
-  if (isLoading) {
-    return <Loading></Loading>;
-  }
-  console.log(data);
-
-  // const data: CoverLetterResponse = {
-  //   coverLetterId: 1, // 자기소개서 id
-  //   summary: {
-  //     totalContentQuestionCount: 4, // 총 문항 수
-  //     contentQuestionStatuses: [
-  //       // 문항별 작성 상태(예: 1번 문항 - 작성 완료)
-  //       { contentNumber: 1, contentStatus: "COMPLETED" },
-  //       { contentNumber: 2, contentStatus: "IN_PROGRESS" },
-  //       { contentNumber: 3, contentStatus: "PENDING" },
-  //       { contentNumber: 4, contentStatus: "PENDING" },
-  //     ],
-  //     companyAnalysisId: 1, // 기업 분석 id
-  //     jobRoleSnapshotId: 1, // 직무 분석 id(null일 수도 있음)
-  //     coverLetterUpdatedAt: "2025-04-24T13:03:00", // 전체 자소서 수정일
-  //   },
-  //   content: {
-  //     contentQuestion: "지원 동기를 적어주세요.", // 자기소개서 질문
-  //     contentNumber: 1, // 자기소개서 문항 번호
-  //     contentLength: 700, // 글자수 제한
-  //     contentDetail: "안녕하세요, 저는 프론트엔드 개발자 지망생입니다.....",
-  //     contentExperienceIds: [1, 2], // 선택한 경험 id
-  //     contentProjectIds: [3], // 선택한 프로젝트 id
-  //     contentFirstPrompt: "이거 이렇게 저렇게 요렇게 하고 싶음", // 작성 요청 시의 처음 프롬프트
-  //     contentStatus: "IN_PROGRESS", // "PENDING": 미작성 | "IN_PROGRESS": 작성 중 | "COMPLETED": 작성 완료,
-  //     contentChatLog: [
-  //       { sender: "USER", message: "유저입니다" },
-  //       { sender: "AI", message: "이렇게 바꿔보세요?" },
-  //     ],
-  //     contentUpdatedAt: "2025-04-23T08:50:37.000", // 개별 자소서 문항의 수정일
-  //   },
-  // };
-  if (!data) {
+}: CoverLetterEditorProps) {
+  if (!CoverLetterData) {
     return;
   }
-  const contentNumber = data.content.contentNumber;
-  const contentDetail = data.content.contentDetail;
-  const contentQuestion = data.content.contentQuestion;
-  const contentLength = data.content.contentLength;
+  console.log(CoverLetterData);
+  const contentNumber = CoverLetterData.contentNumber;
+  const contentDetail = CoverLetterData.contentDetail;
+  const contentQuestion = CoverLetterData.contentQuestion;
+  const contentLength = CoverLetterData.contentLength;
 
   return (
     <div className="bg-white border w-[50rem] rounded-xl px-4 py-4">
