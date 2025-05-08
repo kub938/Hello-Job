9dae5dbf4472b6c869bd9c4dd56775bf06eba8e8
sunny <rjqnrdl5113@naver.com>
2025-05-08 15:26:29 +0900

    refactor: JobRoleSnapshotController principal에서 userId 추출 및 유저 검증 로직 추가
    #217

--- backend/src/main/java/com/ssafy/hellojob/domain/jobrolesnapshot/controller/JobRoleSnapshotController.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/jobrolesnapshot/controller/JobRoleSnapshotController.java
@@ -2,7 +2,9 @@ package com.ssafy.hellojob.domain.jobrolesnapshot.controller;
 
 import com.ssafy.hellojob.domain.jobrolesnapshot.dto.response.JobRoleSnapshotResponseDto;
 import com.ssafy.hellojob.domain.jobrolesnapshot.service.JobRoleSnapshotService;
+import com.ssafy.hellojob.global.auth.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,10 @@ public class JobRoleSnapshotController {
     private final JobRoleSnapshotService jobRoleSnapshotService;
 
     @GetMapping("/{jobRoleSnapshotId}")
-    public JobRoleSnapshotResponseDto getJobRoleSnapshot(@PathVariable Integer jobRoleSnapshotId) {
-        return jobRoleSnapshotService.getJobRoleSnapshot(jobRoleSnapshotId);
+    public JobRoleSnapshotResponseDto getJobRoleSnapshot(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @PathVariable Integer jobRoleSnapshotId) {
+        Integer userId = principal.getUserId();
+        return jobRoleSnapshotService.getJobRoleSnapshot(userId, jobRoleSnapshotId);
     }
 }
