cbe7b66f1e5d3974297dc8287d6504e9ab7464f1
kub938 <kub938@naver.com>
2025-05-08 15:39:41 +0900

    fix: 백엔드 엔드포인트 변경에 따른 api 구조 변경

--- frontend/src/api/coverLetterApi.ts
+++ frontend/src/api/coverLetterApi.ts
@@ -1,14 +1,19 @@
 import { CoverLetterPostRequest } from "@/types/coverLetterTypes";
 import { authApi } from "./instance";
 
+interface sendMessageRequest {
+  contentId: number;
+  userMessage: string;
+  contentDetail: string;
+}
+
 export const coverLetterApi = {
   postCoverLetter: (postCoverLetterRequest: CoverLetterPostRequest) => {
     return authApi.post("/api/v1/cover-letter", postCoverLetterRequest);
   },
-  getCoverLetter: (coverLetterId: number, coverLetterNumber: number) => {
-    return authApi.get(
-      `/api/v1/cover-letter/${coverLetterId}/${coverLetterNumber}`
-    );
+
+  getCoverLetter: (contentId: number) => {
+    return authApi.get(`/api/v1/cover-letter-content/${contentId}`);
   },
 
   draftCoverLetter: (coverLetterId: number, coverLetterNumber: number) => {
@@ -19,9 +24,13 @@ export const coverLetterApi = {
   saveCoverLetter: (coverLetterId: number) => {
     return authApi.patch(`/api/v1/cover-letter/${coverLetterId}`);
   },
-  sendMessage: (coverLetterId: number, coverLetterNumber: number) => {
+  sendMessage: (message: sendMessageRequest) => {
     return authApi.post(
-      `/api/v1/cover-letter/${coverLetterId}/${coverLetterNumber}/chat`
+      `/api/v1/cover-letter-content/${message.contentId}/chat`,
+      {
+        userMessage: message.userMessage,
+        contentDetail: message.contentDetail,
+      }
     );
   },
   deleteCoverLetter: (coverLetterId: number) => {
