8cc1d8036bbde1c6b0812fb145eaae5d9777d61f
WooYeonJu <woojja07@naver.com>
2025-05-09 12:49:22 +0900

    feat: 문항 선택 면접 시작

--- backend/src/main/java/com/ssafy/hellojob/domain/interview/controller/InterviewController.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/interview/controller/InterviewController.java
@@ -1,15 +1,13 @@
 package com.ssafy.hellojob.domain.interview.controller;
 
 import com.ssafy.hellojob.domain.interview.dto.response.QuestionListResponseDto;
+import com.ssafy.hellojob.domain.interview.dto.response.SelectInterviewStartResponseDto;
 import com.ssafy.hellojob.domain.interview.service.InterviewService;
 import com.ssafy.hellojob.global.auth.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +36,21 @@ public class InterviewController {
                                                                  @AuthenticationPrincipal UserPrincipal userPrincipal){
         List<QuestionListResponseDto> responseDto = interviewService.getCoverLetterQuestionList(coverLetterInterviewId, userPrincipal.getUserId());
         return responseDto;
+    }
+
+    @PostMapping("/select/cs")
+    public SelectInterviewStartResponseDto startCsSelectInterview(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        return interviewService.startCsSelectInterview(userPrincipal.getUserId());
+    }
+
+    @PostMapping("/select/personality")
+    public SelectInterviewStartResponseDto startPersonalitySelectInterview(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        return interviewService.startPersonalitySelectInterview(userPrincipal.getUserId());
+    }
+
+    @PostMapping("/select/cover-letter")
+    public SelectInterviewStartResponseDto startCoverLetterSelectInterview(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        return interviewService.startCoverLetterSelectInterview(userPrincipal.getUserId());
     }
 
 }
