2ef9bd897d927904e1f03391b708746dcb2c4c2a
kub938 <kub938@naver.com>
2025-05-09 17:42:32 +0900

    refactor: AddCompanyAnalysisModal,  AddJobAnalysisModal 통합

--- frontend/src/pages/CoverLetterAnalysis/AddModal/AddAnalysisModal.tsx
+++ frontend/src/pages/CoverLetterAnalysis/AddModal/AddAnalysisModal.tsx
@@ -1,11 +1,13 @@
 import CorporateResearch from "@/pages/CorporateResearch/CorporateResearch";
+import JobResearch from "@/pages/JobResearch/JobResearch";
 import { useSelectCompanyStore } from "@/store/coverLetterAnalysisStore";
 
 export interface CompanyAnalysisModalProps {
   onClose: () => void;
+  type: "company" | "job";
 }
 
-function AddCompanyAnalysisModal({ onClose }: CompanyAnalysisModalProps) {
+function AddAnalysisModal({ onClose, type }: CompanyAnalysisModalProps) {
   const { company } = useSelectCompanyStore();
 
   const handleOverlayClick = (e: React.MouseEvent) => {
@@ -17,13 +19,17 @@ function AddCompanyAnalysisModal({ onClose }: CompanyAnalysisModalProps) {
   return (
     <div onClick={handleOverlayClick} className="modal-overlay">
       <div className="modal-container h-[80%]">
-        <CorporateResearch
-          type="modal"
-          companyId={company.companyId}
-        ></CorporateResearch>
+        {type === "company" ? (
+          <CorporateResearch
+            type="modal"
+            companyId={company.companyId}
+          ></CorporateResearch>
+        ) : (
+          <JobResearch type="modal" companyId={company.companyId} />
+        )}
       </div>
     </div>
   );
 }
 
-export default AddCompanyAnalysisModal;
+export default AddAnalysisModal;
