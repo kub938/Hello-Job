ff7684f5d340f6edabf1f127cdcf9b5fd5dd26ab
imjh990614 <imjh990614@gmail.com>
2025-05-11 19:14:46 +0900

    feat: 마이페이지 북마크한 기업 분석 목록 출력 페이지 개발

--- frontend/src/pages/Mypage/components/BookmarkedCompanies.tsx
+++ frontend/src/pages/Mypage/components/BookmarkedCompanies.tsx
@@ -1,16 +1,72 @@
+import { useState } from "react";
 import MypageHeader from "./MypageHeader";
+import { useQuery } from "@tanstack/react-query";
+import { getCompanyBookMarksAll } from "@/api/mypageApi";
+import DetailModal from "@/components/Common/DetailModal";
+import ReadCorporate from "@/pages/CorporateResearch/components/ReadCorporate";
+import CorporateReportCard from "@/pages/CorporateResearch/components/CorporateReportCard";
 
 function BookmarkedCompanies() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [researchCompanyId, setResearchCompanyId] = useState<number>(1);
+
+  const { data: bookmarkedCompanyListData, isLoading } = useQuery({
+    queryKey: ["bookmarkedCompanyList"],
+    queryFn: async () => {
+      const response = await getCompanyBookMarksAll();
+      return response.data;
+    },
+  });
+
+  const openReadModal = (id: number) => {
+    setResearchCompanyId(id);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="flex-1 p-4 md:p-6 md:ml-56 transition-all duration-300">
-      <MypageHeader title="기업 분석 즐겨찾기" />
-      <div className="bg-white rounded-lg shadow-sm p-6">
-        <h2 className="text-xl font-semibold mb-4">기업 분석 목록</h2>
-        <div className="bg-gray-50 p-4 rounded-md text-center">
-          <p>북마크한 기업 목록을 확인할 수 있습니다.</p>
-          <p>관심 있는 기업을 한눈에 관리해 보세요.</p>
-        </div>
+      <MypageHeader title="기업 분석 북마크" />
+      <h2 className="text-xl font-semibold mb-4">
+        내가 북마크한 기업 분석을 모아 볼 수 있습니다.
+      </h2>
+      <div className="flex justify-start gap-4 w-[1164px] mx-auto flex-wrap">
+        {isLoading ? (
+          <div>로딩 중...</div>
+        ) : bookmarkedCompanyListData &&
+          bookmarkedCompanyListData.length > 0 ? (
+          bookmarkedCompanyListData.map((corporateReport) => (
+            <CorporateReportCard
+              key={corporateReport.companyAnalysisId}
+              onClick={() => {
+                openReadModal(corporateReport.companyAnalysisId);
+              }}
+              companyName={corporateReport.companyName}
+              createdAt={corporateReport.createdAt}
+              companyViewCount={corporateReport.companyViewCount}
+              companyLocation={corporateReport.companyLocation}
+              companyAnalysisBookmarkCount={
+                corporateReport.companyAnalysisBookmarkCount
+              }
+              bookmark={corporateReport.bookmark}
+              dartCategory={corporateReport.dartCategory}
+            />
+          ))
+        ) : (
+          <div className="text-center text-gray-500 text-lg h-[180px] w-[210px] flex flex-col items-center justify-center">
+            <h1>검색 결과가 없습니다.</h1>
+            <h1 className="text-base mt-2">기업 분석을 진행해주세요.</h1>
+          </div>
+        )}
       </div>
+      {isModalOpen && (
+        <DetailModal isOpen={isModalOpen} onClose={closeModal}>
+          <ReadCorporate onClose={closeModal} id={researchCompanyId} />
+        </DetailModal>
+      )}
     </div>
   );
 }
