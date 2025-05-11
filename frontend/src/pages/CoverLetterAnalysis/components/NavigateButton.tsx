8a06cc5bfce8239573986ee199d0135f2f11bf5b
kub938 <kub938@naver.com>
2025-05-11 20:59:22 +0900

    chore: 안스는 handle함수 제거

--- frontend/src/pages/CoverLetterAnalysis/components/NavigateButton.tsx
+++ frontend/src/pages/CoverLetterAnalysis/components/NavigateButton.tsx
@@ -16,7 +16,7 @@ function NavigateButton({
 
   return (
     <div className="flex justify-end mt-4 gap-5">
-      {nowStep !== 0 && handleStep && (
+      {nowStep !== 0 && (
         <Button
           onClick={() => handleStep("before")}
           variant={"white"}
