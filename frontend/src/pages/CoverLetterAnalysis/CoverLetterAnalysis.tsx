e060558ee8c04a662215cded313165b3de3b6537
kub938 <kub938@naver.com>
2025-05-11 21:43:03 +0900

    chore: url 삭제

--- frontend/src/pages/CoverLetterAnalysis/CoverLetterAnalysis.tsx
+++ frontend/src/pages/CoverLetterAnalysis/CoverLetterAnalysis.tsx
@@ -8,8 +8,6 @@ import NavigateButton from "./components/NavigateButton";
 
 function CoverLetter() {
   const [nowStep, setNowStep] = useState(0);
-  const pathname = useLocation().pathname;
-  const navigate = useNavigate();
   const [createModalOpen, setCreateModalOpen] = useState(false);
 
   const handleStep = useCallback((type: "next" | "before") => {
@@ -18,36 +16,36 @@ function CoverLetter() {
     } else {
       setNowStep((prev) => prev - 1);
     }
-    navigate(stepUrl[nowStep]);
+    // navigate(stepUrl[nowStep]);
   }, []);
 
   const handleOpenCreateModal = () => {
     setCreateModalOpen(true);
   };
-  useEffect(() => {
-    switch (pathname) {
-      case stepUrl[0]:
-        setNowStep(0);
-        break;
-      case stepUrl[1]:
-        setNowStep(1);
-        break;
-      case stepUrl[2]:
-        setNowStep(2);
-        break;
-      case stepUrl[3]:
-        setNowStep(3);
-        break;
-    }
-  }, [pathname]);
+  // useEffect(() => {
+  //   switch (pathname) {
+  //     case stepUrl[0]:
+  //       setNowStep(0);
+  //       break;
+  //     case stepUrl[1]:
+  //       setNowStep(1);
+  //       break;
+  //     case stepUrl[2]:
+  //       setNowStep(2);
+  //       break;
+  //     case stepUrl[3]:
+  //       setNowStep(3);
+  //       break;
+  //   }
+  // }, [pathname]);
 
-  const stepUrl = [
-    "/cover-letter",
-    "/cover-letter/select-company",
-    "/cover-letter/select-job",
-    "/cover-letter/input-question",
-    `/cover-letter/1`,
-  ];
+  // const stepUrl = [
+  //   "/cover-letter",
+  //   "/cover-letter/select-company",
+  //   "/cover-letter/select-job",
+  //   "/cover-letter/input-question",
+  //   `/cover-letter/1`,
+  // ];
 
   return (
     <>
