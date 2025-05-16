bd3a7d8e1587907413fd1c8ef96e89730a1f5789
WooYeonJu <woojja07@naver.com>
2025-05-16 10:11:04 +0900

    refactor: 누락된 유효성 검사 추가

--- backend/src/main/java/com/ssafy/hellojob/domain/interview/service/InterviewService.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/interview/service/InterviewService.java
@@ -76,8 +76,8 @@ public class InterviewService {
 
     private final Integer QUESTION_SIZE = 5;
 
-//    @Value("${FFPROBE_PATH}")
-//    private String ffprobe_path;
+    @Value("${FFPROBE_PATH}")
+    private String ffprobe_path;
 
     @Value("${OPENAI_API_URL}")
     private String openAiUrl;
@@ -721,8 +721,6 @@ public class InterviewService {
     public String getVideoDurationWithFFprobe(MultipartFile videoFile) throws IOException, InterruptedException {
         File tempFile = File.createTempFile("upload", ".mp4");
         videoFile.transferTo(tempFile);
-
-        String ffprobe_path = "ffprobe.exe";
 
         ProcessBuilder pb = new ProcessBuilder(
                 ffprobe_path,
