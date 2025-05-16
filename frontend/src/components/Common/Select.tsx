144c00ca995cbb08a6821bb9877a3ee092e080f4
sunny <rjqnrdl5113@naver.com>
2025-05-16 16:17:17 +0900

    feat: 일정 관리에서 자기소개서 목록 가져오기 구현
    #184

--- frontend/src/components/Common/Select.tsx
+++ frontend/src/components/Common/Select.tsx
@@ -1,27 +1,45 @@
-import { Fragment } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { FaChevronDown } from "react-icons/fa";
+import { Fragment } from "react";
+import { getScheduleCoverLettersResponse } from "@/types/scheduleApiTypes";
+import { format, parseISO } from "date-fns";
 
 interface SelectProps {
-  options: { value: string; label: string }[];
-  value: number;
-  onChange: (value: number) => void;
+  options: getScheduleCoverLettersResponse[];
+  value: number | "none" | null;
+  onChange: (value: number | "none" | null) => void;
   label?: string;
   require?: boolean;
   placeholder?: string;
   width?: string;
 }
 
-function Select({
+function formatDatetoDot(date: string) {
+  try {
+    return format(parseISO(date), "yyyy.MM.dd");
+  } catch (error) {
+    return date;
+  }
+}
+
+export default function Select({
   options,
   value,
   onChange,
   label,
   require,
-  placeholder = "선택해주세요",
+  placeholder = "일정에 맞는 자기소개서를 선택해주세요",
   width = "100%",
 }: SelectProps) {
-  const selectedOption = options.find((option) => option.value === value.toString());
+  const selectedOption =
+    value === "none"
+      ? { coverLetterId: "none", coverLetterTitle: "선택 안 함", updatedAt: "" }
+      : options.find((option) => option.coverLetterId === value);
+
+  const modifiedOptions = [
+    { coverLetterId: "none", coverLetterTitle: "선택 안 함", updatedAt: "" },
+    ...options,
+  ];
 
   return (
     <div className="my-2" style={{ width }}>
@@ -30,61 +48,80 @@ function Select({
           {label} {require && <span className="text-destructive">*</span>}
         </div>
       )}
+
       <Listbox value={value} onChange={onChange}>
-        <div className="relative pb-3">
-          <Listbox.Button className="relative w-full cursor-pointer rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left text-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent">
-            <span className="block truncate">
-              {selectedOption ? selectedOption.label : placeholder}
-            </span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <FaChevronDown className="h-4 w-4 text-gray-400" />
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              {options.map((option) => (
-                <Listbox.Option
-                  key={option.value}
-                  className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 pl-3 pr-9 ${
-                      active ? "bg-primary text-white" : "text-gray-900"
-                    }`
-                  }
-                  value={option.value}
-                >
-                  {({ selected, active }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? "font-medium" : "font-normal"
+        {({ open }) => (
+          <div className="relative pb-3">
+            <Listbox.Button className="relative w-full cursor-pointer rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left text-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent">
+              <span className="block truncate">
+                {selectedOption
+                  ? `${selectedOption.coverLetterTitle}` +
+                    (selectedOption.updatedAt
+                      ? `(최종 수정일: ${formatDatetoDot(
+                          selectedOption.updatedAt
+                        )})`
+                      : "")
+                  : placeholder}
+              </span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <FaChevronDown className="h-4 w-4 text-gray-400" />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              as={Fragment}
+              show={open}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-40 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {modifiedOptions.map((option) => (
+                  <Listbox.Option
+                    key={option.coverLetterId}
+                    value={option.coverLetterId}
+                  >
+                    {({ selected, active }) => (
+                      <div
+                        className={`relative cursor-pointer select-none py-2 pl-3 pr-9 ${
+                          active ? "bg-primary text-white" : "text-gray-900"
                         }`}
                       >
-                        {option.label}
-                      </span>
-                      {selected && (
-                        <span
-                          className={`absolute inset-y-0 right-0 flex items-center pr-3 ${
-                            active ? "text-white" : "text-primary"
-                          }`}
-                        >
-                          ✓
-                        </span>
-                      )}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
+                        <div className="flex flex-row gap-2 items-center">
+                          <div className="truncate text-sm font-medium">
+                            {option.coverLetterTitle}
+                          </div>
+                          <div
+                            className={`text-xs ${
+                              active ? "text-gray-100" : "text-gray-500"
+                            }`}
+                          >
+                            {option.updatedAt
+                              ? `최종 수정일: ${formatDatetoDot(
+                                  option.updatedAt
+                                )}`
+                              : ""}
+                          </div>
+                        </div>
+
+                        {selected && (
+                          <span
+                            className={`absolute inset-y-0 right-0 flex items-center pr-3 ${
+                              active ? "text-white" : "text-primary"
+                            }`}
+                          >
+                            ✓
+                          </span>
+                        )}
+                      </div>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        )}
       </Listbox>
     </div>
   );
 }
-
-export default Select;
