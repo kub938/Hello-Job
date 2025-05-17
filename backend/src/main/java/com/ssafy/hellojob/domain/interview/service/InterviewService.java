27e92a60639d11f666dc6f0677fe0bd881c50d0d
WooYeonJu <woojja07@naver.com>
2025-05-17 22:23:04 +0900

    style: 한 문항 종료 오류 수정용 로그 추가

--- backend/src/main/java/com/ssafy/hellojob/domain/interview/service/InterviewService.java
+++ backend/src/main/java/com/ssafy/hellojob/domain/interview/service/InterviewService.java
@@ -656,6 +656,9 @@ public class InterviewService {
         InterviewAnswer interviewAnswer = interviewReadService.findInterviewAnswerByIdOrElseThrow(interviewAnswerId);
         InterviewVideo interviewVideo = interviewReadService.findInterviewVideoByIdOrElseThrow(interviewAnswer.getInterviewVideo().getInterviewVideoId());
 
+        log.debug("interviewAnswerId: {}", interviewAnswer.getInterviewAnswerId());
+        log.debug("interviewVideoId: {}", interviewVideo.getInterviewVideoId());
+
         if (interviewAnswer.getInterviewQuestionCategory().name().equals("자기소개서면접")) {
             CoverLetterInterview coverLetterInterview = interviewReadService.findCoverLetterInterviewById(interviewVideo.getCoverLetterInterview().getCoverLetterInterviewId());
             log.debug("자소서 invalid");
