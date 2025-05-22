d14c648e7d2bb711e9e29a74119a08a139d20cfd
WooYeonJu <woojja07@naver.com>
2025-05-22 23:01:18 +0900

    refactor: 면접 피드백 생성 마지막 문항일 시 커밋 후 피드백 생성 요청으로 수정

--- backend/src/main/java/com/ssafy/hellojob/global/common/commitevent/entity/InterviewAnswerSavedEvent.java
+++ backend/src/main/java/com/ssafy/hellojob/global/common/commitevent/entity/InterviewAnswerSavedEvent.java
@@ -4,6 +4,7 @@ import com.ssafy.hellojob.domain.interview.entity.InterviewAnswer;
 
 public class InterviewAnswerSavedEvent {
     private final InterviewAnswer interviewAnswer;
+    private Integer userId;
 
     public InterviewAnswerSavedEvent(InterviewAnswer interviewAnswer) {
         this.interviewAnswer = interviewAnswer;
@@ -11,5 +12,9 @@ public class InterviewAnswerSavedEvent {
 
     public InterviewAnswer getInterviewAnswer() {
         return interviewAnswer;
+    }
+
+    public Integer getUserId() {
+        return userId;
     }
 }
