9b452f5fa0486ffd14e44776b60a59fa37276e97
imjh990614 <imjh990614@gmail.com>
2025-05-24 22:04:44 +0900

    hotfix: parser 예외처리

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -35,11 +35,6 @@ function Login() {
       <div className="container relative h-screen flex-col items-center justify-center lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900"></div>
-          <Link to="/">
-            <div className="relative z-20 flex items-center text-xl font-medium">
-              Hello Job
-            </div>
-          </Link>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="typing-demo text-4xl font-gothic">
